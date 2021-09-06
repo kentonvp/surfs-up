e15140d8d3b5a820c61d16dc5ce33f42929a61fb
Kenton Van Peursem <kenton.vanpeursem@moremath.ai>
2021-09-06 17:55:59 -0500

    feat: empty database design

---
+++ surfsup/surfline/database.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+class SurflineDatabase:
+
+    def __init__(self, db_name:str):
+        pass
+
+    def add_record(self, record):
+        pass
+
+    def del_record(self, record):
+        pass
+
+    def merge_record(self, record, by:str="name"):
+        pass
+
+    def select(self, by:str="name"): # -> record:
+        pass
