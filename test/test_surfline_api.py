840fe41af5dff40eedf495e4c301b32cdc414ed2
Kenton Van Peursem <kenton.vanpeursem@moremath.ai>
2021-09-08 09:29:25 -0500

    feat: spot lookup table

---
+++ test/test_surfline_api.py
@@ -0,0 +1,27 @@
+import unittest
+
+from surfsup.surfline.database import SpotRecord, SurflineSpotDB
+
+class TestSurflineAPI(unittest.TestCase):
+
+    def test_database_not_found(self):
+        pass
+
+    def test_load_database(self):
+        pass
+
+    def test_add_record(self):
+        pass
+
+    def test_del_record(self):
+        pass
+
+    def test_merge_record(self):
+        pass
+
+    def test_select_record(self):
+        pass
+
+
+if __name__ == '__main':
+    unittest.main()
