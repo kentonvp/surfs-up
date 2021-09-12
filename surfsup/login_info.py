c32e6677457c2b18288b95af2b74f9f291e43016
Kenton Van Peursem <kenton.vanpeursem@moremath.ai>
2021-09-11 22:55:10 -0500

    fix: design change--pass in credentials

--- surfsup/login_info.py
+++ surfsup/login_info.py
@@ -1,15 +1,9 @@
-from os import getenv
-from surfsup.excepts import UserInfoNotFoundException
 
 class LoginInfo:
 
-    def __init__(self):
+    def __init__(self, username:str, password:str):
         """Initialize user information from environment variables."""
-        env_username:str = "SURFLINE_USERNAME"
-        env_password:str = "SURFLINE_PASSWORD"
-        if getenv(env_username) == None or getenv(env_password) == None:
-            raise UserInfoNotFoundException
 
-        self.username:str = str(getenv(env_username))
-        self.password:str = str(getenv(env_password))
+        self.username:str = username
+        self.password:str = password
 
