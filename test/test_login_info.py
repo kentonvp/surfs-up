8457e66768afcac553c3939a73e1639d4a15632d
Kenton Van Peursem <kenton.vanpeursem@moremath.ai>
2021-09-11 22:55:29 -0500

    fix: login tests

--- test/test_login_info.py
+++ test/test_login_info.py
@@ -1,33 +1,16 @@
 import os
 import unittest
 
-from surfsup.excepts import UserInfoNotFoundException
 from surfsup.login_info import LoginInfo
 
 
 class TestLoginInfo(unittest.TestCase):
-    env_username:str = "SURFLINE_USERNAME"
-    env_password:str = "SURFLINE_PASSWORD"
-
-
-    def test_not_found(self) -> None:
-        if os.getenv(self.env_username) != None:
-            os.environ.pop(self.env_username)
-
-        if os.getenv(self.env_password) != None:
-            os.environ.pop(self.env_password)
-
-        with self.assertRaises(UserInfoNotFoundException):
-            LoginInfo()
-
 
     def test_initialize(self) -> None:
         fake_username:str = "foo"
         fake_password:str = "foobar"
-        os.environ[self.env_username] = fake_username
-        os.environ[self.env_password] = fake_password
 
-        login = LoginInfo()
+        login = LoginInfo(fake_username, fake_password)
 
         self.assertEqual(login.username, fake_username)
         self.assertEqual(login.password, fake_password)
