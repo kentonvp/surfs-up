411573a320cf8210f198e0aa070c3626c711914a
Calvin Tran <87503872+PhoMachine@users.noreply.github.com>
2021-09-22 14:12:36 -0700

    Add files via upload

---
+++ surfsup/main.py
@@ -0,0 +1,11 @@
+import os
+import telebot
+
+my_secret = os.environ['API_KEY']
+bot = telebot.TeleBot(my_secret)
+
+@bot.message_handler(commands=["Hello"])
+def greet(message):
+  bot.send_message(message.chat.id, "Hey! Are you ready to shred?")
+
+bot.polling()
